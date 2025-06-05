344cee391668353dc9b0f2ec538e9c6d9677004d
ThijsUGent <thijs.duvillard@ugent.be>
2025-06-05 08:56:15 +0000

    configuration choice

--- tool_modules/cluster_configuration.py
+++ tool_modules/cluster_configuration.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 
 def cluster_configuration():
-    st.text("Under construction")
+    choice = st.radio("Select an option", [
+                      "Create a cluster", "Upload a cluster"])
+
+    if choice == "Create a cluster":
+        st.text("Under construction")
+    if choice == "Upload a cluster":
+        st.text("Under construction")
